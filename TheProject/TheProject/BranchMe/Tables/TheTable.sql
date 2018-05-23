91a8cc7d490bc72182ea5eb6a469e8e143bf59c3
Markus Delhofen <markus.delhofen@groupm.com>
2018-05-23 08:00:03 +0200

    Initial Version

---
+++ TheProject/TheProject/BranchMe/Tables/TheTable.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [BranchMe].[TheTable]
+(
+	[TheTableId] BIGINT NOT NULL IDENTITY(1,1),
+	[TheColumn]  NVARCHAR(100) NOT NULL,
+	CONSTRAINT [PK_TheTable] PRIMARY KEY ([TheTableId])
+)
+GO
+
+CREATE INDEX [IX_TheTable_TheColumn] 
+	      ON [BranchMe].[TheTable] (
+	[TheColumn]
+) WITH (DATA_COMPRESSION = PAGE)
+GO
